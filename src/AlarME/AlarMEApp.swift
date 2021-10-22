9b34f5ce574dd1b732839d91822c5e5295d529f2
Runinterface <runinterface@gmail.com>
2021-10-22 09:54:05 +0300

    Init

---
+++ src/AlarME/AlarMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarMEApp.swift
+//  AlarME
+//
+//  Created by Runinterface on 22.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct AlarMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
